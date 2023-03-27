b131848a8826c3183ce06864e1d9a42f5b260b2c
Paranthaman-Palani <paranthaman.palani@cdw.com>
2023-03-27 23:56:30 +0530

    commit

--- Assignment-5/Flyweight.java
+++ Assignment-5/Flyweight.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 public class Flyweight {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
-			List<String> fruitBasket = Arrays.asList("Apple", "Mango", "Bannan", "Grapes");
-			System.out.println(fruitBasket);
+			List<String> angryBird = Arrays.asList("blue", "green", "red", "yellow");
+			System.out.println(angryBird);
 			while (true) {
-				System.out.println("Take Fruits:");
+				System.out.println("Select A AngryBird:");
 				String paintColor = input.next();
 				ColorBrush colorBrush = (ColorBrush) PaintFactory.getColor(paintColor);
 				colorBrush.fruitBasket();
@@ -20,32 +20,32 @@ public class Flyweight {
 	}
 }
 
-interface Brush {
+interface AngryBird {
 	void fruitBasket();
 }
 
-class ColorBrush implements Brush {
+class ColorBrush implements AngryBird {
 	private String paintColor;
 
 	public ColorBrush(String paintColor) {
 		this.paintColor = paintColor;
 	}
 	public void fruitBasket() {
-		System.out.println("Picked Fruit =" + paintColor);
+		System.out.println("Selected Angry Bird =" + paintColor);
 	}
 }
 
 class PaintFactory {
 	private static final HashMap<String, ColorBrush> colorMap = new HashMap<>();
 
-	public static Brush getColor(String paintColor) {
+	public static AngryBird getColor(String paintColor) {
 		ColorBrush colorBrush = (ColorBrush) colorMap.get(paintColor);
 		if (colorBrush == null) {
 			colorBrush = new ColorBrush(paintColor);
 			colorMap.put(paintColor, colorBrush);
-			System.out.println("Taken Fruit : " + paintColor);
+			System.out.println("Angry Bird color : " + paintColor);
 		}
-		System.out.println("Fruits Basket = "+colorMap);
+		System.out.println("Angry Birds in Game= "+colorMap);
 		return colorBrush;
 	}
 
