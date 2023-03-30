945fdd0fd8a676c30cd2046c3f79d5306f625f09
Paranthaman-Palani <paranthaman.palani@cdw.com>
2023-03-30 12:57:04 +0530

    commit

--- Assignment-3/MultiThreading7.java
+++ Assignment-3/MultiThreading7.java
@@ -6,10 +6,18 @@ import java.util.concurrent.Executors;
 public class MultiThreading7 {
 	public static void main(String[] args) {
 		Counting count = new Counting();
-		ExecutorService es = Executors.newFixedThreadPool(1);
+		ExecutorService es = Executors.newFixedThreadPool(2);
 		es.execute(() -> {
 			try {
 				count.counter();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		es.execute(() -> {
+			try {
+				count.message();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -25,9 +33,15 @@ class Counting {
 		for (int i = 1; i <= 100; i++) {
 			System.out.println(i);
 			Thread.sleep(1000);
-			if (i % 10 == 0) {
+			
+		}
+	}
+	public void message() throws InterruptedException {
+		for (int i = 1; i <= 10; i++) {
+			
+			Thread.sleep(10000);
+			
 				System.out.println("Hello World");
-			}
 		}
 	}
 }
